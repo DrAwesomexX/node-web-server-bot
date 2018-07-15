b1b73c673147d6d69a51727bfff626a5a1047a17
Manav <manavbajaj1997@gmail.com>
2018-07-15 23:16:03 +0530

    initial

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs');
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'chatbot',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server is up on port: ${port}`);
+});
